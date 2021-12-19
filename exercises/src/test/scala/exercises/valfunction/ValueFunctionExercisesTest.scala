d3e7c18fba933f8d578565b18d95d6273dd4a4e3
ryskit <ryosukekida.71@gmail.com>
2021-12-19 16:40:18 +0900

    [Value functions] Done Value Function Exercise

--- exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
@@ -11,15 +11,39 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   /////////////////////////////////////////////////////
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits examples") {
+  test("selectDigits examples") {
     assert(selectDigits("hello4world-80") == "480")
     assert(selectDigits("welcome") == "")
   }
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits length is smaller") {
+  test("selectDigits length is smaller") {
     forAll { (text: String) =>
       assert(selectDigits(text).length <= text.length)
+    }
+  }
+
+  test("selectDigits only returns numbers") {
+    forAll { (text: String) =>
+      selectDigits(text).foreach(c => assert(c.isDigit))
+    }
+  }
+
+  test("secret example") {
+    assert(secret("Welcome123") == "**********")
+  }
+
+  test("secret PBT") {
+    forAll { (text: String) =>
+      val once  = secret(text)
+      val twice = secret(secret(text))
+      assert(once == twice)
+    }
+  }
+
+  test("isValidUsername") {
+    forAll { (username: String) =>
+      assert(isValidUsername(username.reverse) == isValidUsername(username))
     }
   }
 
@@ -27,4 +51,22 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   // Exercise 2: Point
   ///////////////////////
 
+  test("Point isPositive") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      assert(Point(x.max(0), y.max(0), z.max(0)).isPositive)
+    }
+  }
+
+  test("Point isEven") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      val even: (Int) => Int = (a: Int) => if (a % 2 == 0) a else a + 1
+      assert(Point(even(x), even(y), even(z)).isEven)
+    }
+  }
+
+  test("Point forAll") {
+    forAll { (x: Int, y: Int, z: Int, predicate: Int => Boolean) =>
+      Point(x, y, z).forAll(predicate) == List(x, y, z).forall(predicate)
+    }
+  }
 }
