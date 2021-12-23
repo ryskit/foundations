d7816b6cae095d0ad9f175ef216a1f34b8421f7f
ryskit <ryosukekida.71@gmail.com>
2021-12-23 23:03:04 +0900

    [Parallel data processing] done Iteration with for loops

--- exercises/src/main/scala/exercises/dataprocessing/ForLoopExercises.scala
+++ exercises/src/main/scala/exercises/dataprocessing/ForLoopExercises.scala
@@ -2,20 +2,14 @@ package exercises.dataprocessing
 
 object ForLoopExercises {
 
-  def sum(numbers: List[Int]): Int = {
-    var total = 0
-
-    for (number <- numbers)
-      total += number
-
-    total
-  }
+  def sum(numbers: List[Int]): Int =
+    foldLeft(numbers, 0)(_ + _)
 
   // a. Implement `size` using a mutable state and a for loop
   // such as size(List(2,5,1,8)) == 4
   // and     size(Nil) == 0
   def size[A](items: List[A]): Int =
-    ???
+    foldLeft(items, 0)((b, _) => b + 1)
 
   // b. Implement `min` using a mutable state and a for loop
   // such as min(List(2,5,1,8)) == Some(1)
@@ -24,7 +18,10 @@ object ForLoopExercises {
   // * Some when there is a value and
   // * None when there is no value (a bit like null)
   def min(numbers: List[Int]): Option[Int] =
-    ???
+    foldLeft(numbers, None: Option[Int]) {
+      case (None, number)             => Some(number)
+      case (Some(currentMin), number) => Some(currentMin min number)
+    }
 
   // c. Implement `wordCount` using a mutable state and a for loop.
   // `wordCount` compute how many times each word appears in a `List`
@@ -33,12 +30,19 @@ object ForLoopExercises {
   // Note: You can lookup an element in a `Map` with the method `get`
   // and you can upsert a value using `updated`
   def wordCount(words: List[String]): Map[String, Int] =
-    ???
+    foldLeft(words, Map.empty[String, Int]) { (frequencies, word) =>
+      val frequency = frequencies.getOrElse(word, 0)
+      frequencies.updated(word, frequency + 1)
+    }
 
   // d. `sum`, `size`, `min` and `wordCount` are quite similar.
   // Could you write a higher-order function that captures this pattern?
   // How would you call it?
-  def pattern = ???
+  def foldLeft[From, To](items: List[From], defaultValue: To)(combine: (To, From) => To): To = {
+    var state = defaultValue
+    for (item <- items) state = combine(state, item)
+    state
+  }
 
   // e. Refactor `sum`, `size`, `min` and `wordCount` using the higher-order
   // function you defined above.
--- exercises/src/test/scala/exercises/dataprocessing/ForLoopExercisesTest.scala
+++ exercises/src/test/scala/exercises/dataprocessing/ForLoopExercisesTest.scala
@@ -17,19 +17,53 @@ class ForLoopExercisesTest extends AnyFunSuite with ScalaCheckDrivenPropertyChec
     }
   }
 
-  ignore("size") {
+  test("size") {
     assert(size(List(2, 5, 1, 8)) == 4)
     assert(size(Nil) == 0)
   }
 
-  ignore("min") {
+  test("size and concat") {
+    forAll { (list1: List[Int], list2: List[Int]) =>
+      val size1 = list1.size
+      val size2 = list2.size
+      assert(size(list1 ++ list2) == size1 + size2)
+    }
+  }
+
+  test("min") {
     assert(min(List(2, 5, 1, 8)) == Some(1))
     assert(min(Nil) == None)
   }
 
-  ignore("wordCount") {
+  test("min returns a value smaller than all elements in input list") {
+    forAll { (numbers: List[Int]) =>
+      for {
+        minValue <- min(numbers)
+        number   <- numbers
+      } assert(minValue <= number)
+    }
+  }
+
+  test("min returns a value that belongs to the input list") {
+    forAll { (numbers: List[Int]) =>
+      min(numbers).foreach(minValue => numbers.contains(minValue))
+    }
+  }
+
+  test("wordCount") {
     assert(wordCount(List("Hi", "Hello", "Hi")) == Map("Hi" -> 2, "Hello" -> 1))
     assert(wordCount(Nil) == Map.empty)
   }
 
+  test("wordCount returns frequencies > 0") {
+    forAll { (words: List[String]) =>
+      wordCount(words).values.foreach(frequencies => assert(frequencies > 0))
+    }
+  }
+
+  test("foldLeft process inputs in orders") {
+    forAll { (numbers: List[Int]) =>
+      assert(foldLeft(numbers, List.empty[Int])((prefix, number) => prefix :+ number) == numbers)
+    }
+  }
 }
